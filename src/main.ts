44ccd1875a853118403efa643f4f9a711528bfd2
alexandradume <alexandra.teodora.dume@stud.ubbcluj.ro>
2023-07-20 12:06:13 +0300

    backend + input wip

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,10 @@
-import Vue from "vue";
-import App from "./App.vue";
-import store from "./store";
-import vuetify from "./plugins/vuetify";
+import Vue from 'vue';
+import App from './App.vue';
+import vuetify from './plugins/vuetify'; // If you are using Vuetify for styling
 
 Vue.config.productionTip = false;
 
 new Vue({
-  store,
-  vuetify,
+  vuetify, // Attach Vuetify to the Vue instance if you are using it for styling
   render: (h) => h(App),
-}).$mount("#app");
+}).$mount('#app');
